98e58174a83f0980be559b9159645a71a95889ad
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-03-29 14:33:20 +0200

    Added comments to models and LogDate to entries

--- JADY/Models/Diary.cs
+++ JADY/Models/Diary.cs
@@ -2,7 +2,13 @@ namespace JADY.Models;
 
 public class Diary
 {
+    /// <summary>
+    /// The name of the diary.
+    /// </summary>
     public string? Name { get; set; }
     
+    /// <summary>
+    /// The entries in the diary.
+    /// </summary>
     public DiaryEntry[]? Entries { get; set; }
 }
--- JADY/Models/DiaryEntry.cs
+++ JADY/Models/DiaryEntry.cs
@@ -8,21 +8,46 @@ public class DiaryEntry
     
     public DiaryEntryStatus Status { get; set; }
     
-    public DateTime StartDate { get; set; }
-
-    public DateTime EndDate { get; set; }
+    /// <summary>
+    /// The date at the time the entry was added.
+    /// </summary>
+    public DateTime LogDate { get; set; }
     
+    /// <summary>
+    /// The start date of an event or a date of a one time entry.
+    /// </summary>
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// The end date of an event. Is useless for a one time entry.
+    /// </summary>
+    public DateTime? EndDate { get; set; }
+    
+    /// <summary>
+    /// E.g. Game/Anime/Misc
+    /// </summary>
+    public string? Category { get; set; }
+    
+    /// <summary>
+    /// E.g. First time playing Factorio
+    /// </summary>
     public string? Title { get; set; }
     
+    /// <summary>
+    /// E.g. I started playing Factorio for the first time on the ... version and its ...
+    /// </summary>
     public string? Content { get; set; }
     
+    /// <summary>
+    /// E.g. [I automated red science, I finnaly destroyed those biter nests, ...]
+    /// </summary>
     public DiarySubEntry[]? SubEntries { get; set; }
 }
 
 public enum DiaryEntryType
 {
-    Normal = 0,
-    Game = 1
+    OneTime = 0,
+    ProlongedEvent = 1
 }
 
 public enum DiaryEntryStatus
