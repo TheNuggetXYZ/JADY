217141d4acd93697154021e13cc79a28f16e8c41
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-03-30 20:01:29 +0200

    Redid diary entry into an openable panel with all planned properties except type and status

--- JADY/Views/DiaryEntry.axaml.cs
+++ JADY/Views/DiaryEntry.axaml.cs
@@ -1,13 +1,30 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace JADY.Views;
 
 public partial class DiaryEntry : UserControl
 {
+    public bool IsDiaryEntryOpen = false;
+    
     public DiaryEntry()
     {
         InitializeComponent();
+        SetVisibilityValues();
+    }
+
+    private void DiaryEntryHeader_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        IsDiaryEntryOpen = !IsDiaryEntryOpen;
+
+        SetVisibilityValues();
+    }
+
+    private void SetVisibilityValues()
+    {
+        ObjectToOpen0.IsVisible = ObjectToOpen0.IsEnabled = IsDiaryEntryOpen;
+        ObjectToOpen1.IsVisible = ObjectToOpen1.IsEnabled = IsDiaryEntryOpen;
     }
 }
