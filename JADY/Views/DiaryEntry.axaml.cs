a5ca4da3e69f0db26aa69c7c382749d8d481a7fc
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-03-30 16:37:11 +0200

    Made diary entry its separate view/user control

---
+++ JADY/Views/DiaryEntry.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace JADY.Views;
+
+public partial class DiaryEntry : UserControl
+{
+    public DiaryEntry()
+    {
+        InitializeComponent();
+    }
+}
