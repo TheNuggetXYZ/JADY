f538465c7c5d9cd721d1396da85ca3bb4f10395e
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-04-05 14:11:15 +0200

    Closing windows by type in WindowManager.cs

--- JADY/Backend/WindowManager.cs
+++ JADY/Backend/WindowManager.cs
@@ -10,6 +10,9 @@ namespace JADY.Backend;
 
 public static class WindowManager
 {
+    
+    private static readonly Dictionary<Type, Window> OpenWindows = new();
+
     public static Window? GetMainWindow()
     {
         if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
@@ -21,9 +24,24 @@ public static class WindowManager
     public static async Task OpenDialogWindow<T>(Window? owner, Action? onCloseAction, object? dataContext) where T : Window, new()
     {
         if (owner == null) return;
+
+        if (OpenWindows.TryGetValue(typeof(T), out var window))
+            return;
         
         T newWindow = new T {DataContext = dataContext};
+        
+        OpenWindows.Add(typeof(T), newWindow);
+        newWindow.Closed += (_, _) => { OpenWindows.Remove(typeof(T)); };
+        
         await newWindow.ShowDialog(owner);
         onCloseAction?.Invoke();
+    }
+
+    public static void CloseWindow<T>() where T : Window
+    {
+        if (OpenWindows.TryGetValue(typeof(T), out var window))
+        {
+            window.Close(); // gets automatically removed from dictionary
+        }
     }
 }
