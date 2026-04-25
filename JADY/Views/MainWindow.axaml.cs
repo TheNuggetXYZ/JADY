212c9f55cf0f8cf0e2ffe0e7ad8ff627830b4210
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-04-25 13:07:28 +0200

    Make the diaries tab control scrollable

--- JADY/Views/MainWindow.axaml.cs
+++ JADY/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace JADY.Views;
 
@@ -7,5 +8,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void TabStrip_OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
+    {
+        if (sender is ScrollViewer sv)
+        {
+            sv.Offset = sv.Offset.WithX(sv.Offset.X - e.Delta.Y * 50);
+            e.Handled = true;
+        }
     }
 }
