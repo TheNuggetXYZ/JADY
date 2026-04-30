6acc97741de98a45edf4f30886d7acdce8fe8e1a
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-04-30 16:37:11 +0200

    Fix expand angle animating when you switch between entries

--- JADY/Views/DiaryEntryControl.axaml.cs
+++ JADY/Views/DiaryEntryControl.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using JADY.ViewModels;
@@ -9,6 +10,21 @@ public partial class DiaryEntryControl : UserControl
     public DiaryEntryControl()
     {
         InitializeComponent();
+        
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    // Fix animations, that are undesired when this object gets recycled/created, taking place when switching between diaries.
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        // temporarily remove transitions
+        var transitions = ExpandAngle.Transitions;
+        ExpandAngle.Transitions = null;
+
+        // force ui update
+        ExpandAngle.InvalidateVisual();
+
+        ExpandAngle.Transitions = transitions;
     }
 
     private void Control_OnClick(object? sender, RoutedEventArgs e)
