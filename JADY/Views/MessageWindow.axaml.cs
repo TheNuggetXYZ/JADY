b14bee22ba1215a4c014fc29785df7a009fe62b6
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-02 18:27:58 +0200

    Close MessageWindow on esc or enter

--- JADY/Views/MessageWindow.axaml.cs
+++ JADY/Views/MessageWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 
 namespace JADY.Views;
@@ -16,4 +17,12 @@ public partial class MessageWindow : Window
     }
 
     private void OkButton_OnClick(object? sender, RoutedEventArgs e) => Close();
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
+            Close();
+        else if (e.Key == Key.Enter)
+            Close();
+    }
 }
