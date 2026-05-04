9c8d504bfb880329ceb88f425c9de4e9e59a93f5
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-04 18:29:26 +0200

    Add MainTabControl templated control with settable LeftPanelContent and RightPanelContent to replace the bloated TabControl in MainWindow

---
+++ JADY/Views/Controls/MainTabControl.axaml.cs
@@ -0,0 +1,45 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+
+namespace JADY.Views.Controls;
+
+public class MainTabControl : TabControl
+{
+    public static readonly StyledProperty<object?> LeftPanelContentProperty =
+        AvaloniaProperty.Register<MainTabControl, object?>(nameof(LeftPanelContent));
+
+    public object? LeftPanelContent
+    {
+        get => GetValue(LeftPanelContentProperty);
+        set => SetValue(LeftPanelContentProperty, value);
+    }
+
+    public static readonly StyledProperty<object?> RightPanelContentProperty =
+        AvaloniaProperty.Register<MainTabControl, object?>(nameof(RightPanelContent));
+
+    public object? RightPanelContent
+    {
+        get => GetValue(RightPanelContentProperty);
+        set => SetValue(RightPanelContentProperty, value);
+    }
+    
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        // Find the ScrollViewer by a Name you set in the XAML
+        var scrollViewer = e.NameScope.Find<ScrollViewer>("PART_TabStripScrollViewer");
+        
+        if (scrollViewer != null)
+        {
+            scrollViewer.PointerWheelChanged += (s, args) =>
+            {
+                var offset = scrollViewer.Offset;
+                scrollViewer.Offset = offset.WithX(offset.X - args.Delta.Y * 50);
+                args.Handled = true;
+            };
+        }
+    }
+}
