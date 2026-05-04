3ca212209efd8554e4c889e074ffd604c3480505
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-04 18:44:43 +0200

    Make settings button call a command instead of a method in code behind that executes that command

--- JADY/Views/Windows/MainWindow.axaml.cs
+++ JADY/Views/Windows/MainWindow.axaml.cs
@@ -14,21 +14,6 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    private void TabStrip_OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
-    {
-        if (sender is ScrollViewer sv)
-        {
-            sv.Offset = sv.Offset.WithX(sv.Offset.X - e.Delta.Y * 50);
-            e.Handled = true;
-        }
-    }
-
-    private void Settings_OnClick(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is MainWindowViewModel vm)
-            vm.Menu_OpenSettingsWindowCommand.Execute(null);
-    }
-
     protected override async void OnClosing(WindowClosingEventArgs e)
     {
         if (_handledUnsavedChanges)
