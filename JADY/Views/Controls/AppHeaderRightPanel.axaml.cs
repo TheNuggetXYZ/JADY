77b49b81ff092db19f5ea712b442af94bf5b471d
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-04 18:54:56 +0200

    Extract AppHeaderRightPanel from MainWindow into its UserControl

---
+++ JADY/Views/Controls/AppHeaderRightPanel.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace JADY.Views.Controls;
+
+public partial class AppHeaderRightPanel : UserControl
+{
+    public AppHeaderRightPanel()
+    {
+        InitializeComponent();
+    }
+}
