6ae12f38c605b64121f201e3bb113b54e7387241
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-05 15:48:39 +0200

    Add EqualsConverter

---
+++ JADY/Views/Converters/EqualsConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace JADY.Views.Converters;
+
+public class EqualsConverter : IValueConverter
+{
+    public int CompareTo { get; set; }
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int i)
+            return i == CompareTo;
+        else if (value is float f)
+            return f == CompareTo;
+
+        return false;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
