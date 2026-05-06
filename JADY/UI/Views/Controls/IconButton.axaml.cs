e0b60e6ed915e07b47bdb2f5aa61f44427c3dca1
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-06 15:04:32 +0200

    Add IconData and IconText properties to IconButton and render them properly

--- JADY/UI/Views/Controls/IconButton.axaml.cs
+++ JADY/UI/Views/Controls/IconButton.axaml.cs
@@ -4,4 +4,23 @@ using Avalonia.Controls.Primitives;
 
 namespace JADY.UI.Views.Controls;
 
-public class IconButton : HoverButton { }
+public class IconButton : HoverButton
+{
+    public static readonly StyledProperty<object?> IconDataProperty =
+        AvaloniaProperty.Register<IconButton, object?>(nameof(IconData));
+
+    public object? IconData
+    {
+        get => GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+    
+    public static readonly StyledProperty<object?> IconTextProperty =
+        AvaloniaProperty.Register<IconButton, object?>(nameof(IconText));
+
+    public object? IconText
+    {
+        get => GetValue(IconTextProperty);
+        set => SetValue(IconTextProperty, value);
+    }
+}
