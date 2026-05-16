6d2d9551e2843579592552842083798e8a543280
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-16 11:29:44 +0200

    chore: add LoadingSaveWindow

---
+++ JADY/UI/Views/Windows/LoadingSaveWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace JADY.UI.Views.Windows;
+
+public partial class LoadingSaveWindow : Window
+{
+    public LoadingSaveWindow()
+    {
+        InitializeComponent();
+    }
+}
