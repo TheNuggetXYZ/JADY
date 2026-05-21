1c671653cac4d3cfe0ffb86860392314b2e58fcb
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-21 21:28:07 +0200

    fix: DI DiaryService circular dependency

--- JADY/Factories/DiaryViewModelFactory.cs
+++ JADY/Factories/DiaryViewModelFactory.cs
@@ -1,13 +1,18 @@
+using System;
 using JADY.Core.Models;
 using JADY.Services;
 using JADY.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace JADY.Factories;
 
-public class DiaryViewModelFactory(IDiaryEntryViewModelFactory diaryEntryViewModelFactory, IWindowService windowService, IDiaryService diaryService) : IDiaryViewModelFactory
+public class DiaryViewModelFactory(IServiceProvider serviceProvider, IDiaryEntryViewModelFactory diaryEntryViewModelFactory, IWindowService windowService) : IDiaryViewModelFactory
 {
     public DiaryViewModel Create(Diary diary)
     {
+        // resolve circular dependency
+        var diaryService = serviceProvider.GetRequiredService<IDiaryService>();
+        
         return new DiaryViewModel(diary, diaryEntryViewModelFactory, windowService, diaryService);
     }
 }
