867b14e1572ccda658506ccaf1e2202b29c37081
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-21 14:55:44 +0200

    chore: add LinkEntryWindow

---
+++ JADY/UI/Views/Dialogs/LinkEntryWindow.axaml.cs
@@ -0,0 +1,42 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using JADY.Core.Data;
+using JADY.Core.Models;
+using JADY.UI.Base;
+using JADY.ViewModels;
+
+namespace JADY.UI.Views.Dialogs;
+
+public partial class LinkEntryWindow : DialogWindow<DiaryEntry>, IDialogInitializable<DiaryEntryViewModel>
+{
+    public void Initialize(DiaryEntryViewModel data)
+    {
+        InitializeComponent();
+        
+        DataContext = data;
+    }
+
+    protected override Optional<DiaryEntry> GetValue()
+    {
+        return new DiaryEntry
+        {
+            Category = EntryCategory.Text,
+            SubCategory = EntrySubcategory.Text,
+            Title = EntryTitle.Text,
+            Content = EntryContent.Text,
+            LogDate = DateTimeOffset.Now,
+            Date = EntryDate.SelectedDate,
+            IsHidden = EntryIsHidden.IsChecked ?? false,
+            Status = EntryStatus.Note
+        };
+    }
+
+    protected override InputElement? FocusedElement() => EntryTitle;
+
+    private async void Submit_OnClick(object? sender, RoutedEventArgs e) => await TrySubmitAsync();
+}
