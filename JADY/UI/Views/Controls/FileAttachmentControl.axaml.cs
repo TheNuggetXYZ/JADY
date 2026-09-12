9731f04e71ab02a2dbd2d53aa6758bea0ef6eeb7
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-09-12 20:33:18 +0200

    feat: add file attachment control

---
+++ JADY/UI/Views/Controls/FileAttachmentControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace JADY.UI.Views.Controls;
+
+public partial class FileAttachmentControl : UserControl
+{
+    public FileAttachmentControl()
+    {
+        InitializeComponent();
+    }
+}
